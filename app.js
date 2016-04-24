69f0e82c6c362cd0546aa736bdbe4b5961c6032a
Viet Le <viethle126@gmail.com>
2016-04-24 01:26:59 -0700

    Added Route /search #3

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 // routes
 var menu = require('./routes/menu');
+var search = require('./routes/search');
 
 app.use(function(req, res, next) {
   console.log(req.url);
   next();
 })
 app.use('/menu', menu);
+app.use('/search', search);
 
 if (!require.main.loaded) {
   var port = process.env.PORT || 1337;
