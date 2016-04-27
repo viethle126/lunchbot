a77fa132ad9ed42b625a9da0bfc7d2b1d8704b2a
Viet Le <viethle126@gmail.com>
2016-04-26 21:48:57 -0700

    User can ask @lunchbot to return a restaurant's menu #5

--- lunchbot.js
+++ lunchbot.js
@@ -5,14 +5,31 @@ var bot = controller.spawn({
   token: process.env.SLACK_BOT_TOKEN
 })
 // custom modules
+var menu = require('./lib/menu');
 var search = require('./lib/search');
 var uptime = require('./lib/uptime');
 
 var qResults = ''; // temporarily storing data, will implement mongodb later
+var qMenu = ''; // temporarily storing data, will implement mongodb later
 var context = {
   dm: ['direct_message'],
   all: ['direct_message', 'direct_mention', 'mention', 'ambient'],
   general: ['direct_message', 'direct_mention', 'mention']
+}
+
+function match(input) {
+  var ref = Number(input);
+  var name = input;
+  var url = '';
+  var found = false;
+  qResults.restaurants.forEach(function(element, index, array) {
+    if (element.ref === ref || element.name.toLowerCase() === name.toLowerCase()) {
+      url = element.eat24;
+      found = true;
+    }
+  })
+  if (found = false) { url = false }
+  return url;
 }
 
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
@@ -38,6 +55,31 @@ controller.hears(['search (.*) near (.*)', 'find (.*) near (.*)', 'list (.*) nea
         bot.reply(message, response);
       }
     })
+  })
+
+controller.hears(['menu for (.*)'],
+  context.general, function(bot, message) {
+    var url = match(message.match[1]);
+    if (url === false) {
+      bot.reply(message, 'Sorry, that restaurant isn\'t in my database.');
+    } else {
+      var promise = new Promise(function(resolve, reject) {
+        menu(promise, resolve, reject, url);
+      })
+
+      promise.then(function(payload) {
+        var total = payload.length;
+        qMenu = {
+          sent: 0,
+          total: total,
+          sections: payload
+        }
+        var header = 'I found ' + total + ' categories. Say \'menu next\' for more.\n'
+        var section = payload[0].join('\n');
+        var response = header + section;
+        bot.reply(message, response);
+      })
+    }
   })
 
 bot.startRTM(function(err, bot, payload) {
